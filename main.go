dc900b011d9508e7ac663cd8472a5896bed67191
Bohdan Pakhomov <shtormless@gmail.com>
2021-02-24 20:11:20 -0500

    add reply keyboard

--- main.go
+++ main.go
@@ -13,7 +13,22 @@ func main() {
 		port      = os.Getenv("PORT")
 		publicURL = os.Getenv("PUBLIC_URL")     // you must add it to your config vars
 		token     = os.Getenv("TELEGRAM_TOKEN") // you must add it to your config vars
+
+		// Universal markup builders.
+		menu = &tb.ReplyMarkup{ResizeReplyKeyboard: true}
+
+		// Reply buttons.
+		btnHello = menu.Text("Say Hi!")
+		btnMemp  = menu.Text("Memorable")
+		btnAbrp  = menu.Text("Abracadabra")
 	)
+
+	menu.Reply(
+		menu.Row(btnHello),
+		menu.Row(btnMemp),
+		menu.Row(btnAbrp),
+	)
+
 	// Set webhook
 	webhook := &tb.Webhook{
 		Listen:   ":" + port,
@@ -35,15 +50,15 @@ func main() {
 		b.Send(m.Sender, "Usage:")
 		b.Send(m.Sender, "/memp - generate memorable password")
 		b.Send(m.Sender, "/abrp - generate abracadabra password")
-		b.Send(m.Sender, "/hello - return 'Hi!'")
+		b.Send(m.Sender, "/ - return 'Hi!'")
 	})
 	// Reply to "/hello" command
-	b.Handle("/hello", func(m *tb.Message) {
+	b.Handle(&btnHello, func(m *tb.Message) {
 		b.Send(m.Sender, "Hi!")
 	})
 
 	// Reply to "/memp" command
-	b.Handle("/memp", func(m *tb.Message) {
+	b.Handle(&btnMemp, func(m *tb.Message) {
 		// get words
 		wrds, _ := GetRandWords(4)
 		// generating memorable password
@@ -52,7 +67,7 @@ func main() {
 	})
 
 	// Reply to "/abrp" command
-	b.Handle("/abrp", func(m *tb.Message) {
+	b.Handle(&btnAbrp, func(m *tb.Message) {
 		b.Send(m.Sender, "Abracadabra pass:")
 		b.Send(m.Sender, String(20))
 	})
